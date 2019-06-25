c88fc05bc6b6420e2692b2f01d7e79d032f44df1
Jsanchez <jsanchez@fedefarma.com>
2019-06-25 16:00:47 +0200

    Refactorización

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
 
 const argv = require('yargs').options({
-        direccion: {
-            alias: 'd',
-            desc: 'Dirección de la ciudad para obtener el clima',
-            demand: true
-        }
-
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
     }
-
-).argv;
+}).argv;
 
 let getInfo = async(direccion) => {
     try {
-        let coors = await lugar.getLugarLatLng(direccion);
-        let temp = await clima.getClima(coors.latitud, coors.longitud);
+        console.log('Dato introducido: ' + direccion);
+        var coors = await lugar.getLugarLatLng(direccion);
+        let temp = await clima.getClima(coors.lat, coors.lng);
 
         return `El clima en ${coors.direccion} es de ${temp}`;
     } catch (e) {
         return `No se puede determinar el clima para ${direccion}`;
-
     }
-
-
-
 }
 
 getInfo(argv.direccion)
     .then(mensaje => console.log(mensaje))
     .catch(e => console.log(e));
+
+function Funcion1() {
+    //console.log("titulo", "mensaje");;
+}
+
+// Funcion1();
