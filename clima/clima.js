fca4d7abe06dbda5e280dd2f6919d21875fa184c
Jsanchez <jsanchez@fedefarma.com>
2019-06-25 16:03:15 +0200

    Refactorización

--- clima/clima.js
+++ clima/clima.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 
 const getClima = async(lat, lng) => {
-    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=52d74726c8c4f3f652068bfe593e2684`)
+    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=52d74726c8c4f3f652068bfe593e2684`);
+
     return resp.data.main.temp;
 
 }
 
 module.exports = {
     getClima
-
 }
