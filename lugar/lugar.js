b1b526e690e2647d570927d60e99b06123efac7e
Jsanchez <jsanchez@fedefarma.com>
2019-06-25 18:14:45 +0200

    Actualizamos servicio de busqueda

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -1,22 +1,50 @@
 const axios = require('axios');
 
-const getLugarLatLng = async(direccion) => {
+const getLugarLatLng = async(dir) => {
 
-    let encodeURL = encodeURI(direccion);
-    let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURL}&key=AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM`)
+    const encodedUlr = encodeURI(dir);
 
-    if (resp.data.status === 'ZERO_RESULTS') {
-        throw new Error(`No se han encontrado resultados para ${direccion}`)
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodedUlr }`,
+        headers: { 'X-RapidAPI-Key': '30e05993bemsh63158fa139412a4p1ee62ajsn03f39c49ccd9' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${ dir }`);
     }
 
-    let location = resp.data.results[0];
-    let coordenadas = location.geometry.location;
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    console.log('Dirección: ' + direccion);
+    console.log('Latitud: ' + lat);
+    console.log('Longitud: ' + lng);
 
     return {
-        direccion: location.formatted_address,
-        latitud: coordenadas.lat,
-        longitud: coordenadas.lng
+        direccion,
+        lat,
+        lng
     }
+
+    // let encodeURL = encodeURI(direccion);
+    // let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURL}&key=AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM`)
+
+    // if (resp.data.status === 'ZERO_RESULTS') {
+    //     throw new Error(`No se han encontrado resultados para ${direccion}`)
+    // }
+
+    // let location = resp.data.results[0];
+    // let coordenadas = location.geometry.location;
+
+    // return {
+    //     direccion: location.formatted_address,
+    //     latitud: coordenadas.lat,
+    //     longitud: coordenadas.lng
+    // }
 
 }
 
