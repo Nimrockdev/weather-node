a310d9fe2c98ddffff9053804359bfecf8e3a8d5
Jsanchez <jsanchez@fedefarma.com>
2019-06-25 18:14:20 +0200

    Preparamos para servicio

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
 
+// Cargar modulos y crear nueva aplicacion
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
+app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
+
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -15,18 +23,42 @@ let getInfo = async(direccion) => {
         var coors = await lugar.getLugarLatLng(direccion);
         let temp = await clima.getClima(coors.lat, coors.lng);
 
-        return `El clima en ${coors.direccion} es de ${temp}`;
+        // return temp;
+
+        return (`El clima en ${coors.direccion} es de ${temp}`);
     } catch (e) {
-        return `No se puede determinar el clima para ${direccion}`;
+        return (`No se puede determinar el clima para ${direccion}`);
     }
 }
+
 
 getInfo(argv.direccion)
     .then(mensaje => console.log(mensaje))
     .catch(e => console.log(e));
 
-function Funcion1() {
-    //console.log("titulo", "mensaje");;
-}
+// function Funcion1() {
+//     console.log('TEST: Pulsamos Botón Función 1');;
+// }
 
 // Funcion1();
+
+
+//Ejemplo: GET http://localhost:8080/items
+// app.get('/Weather', function(req, res, next) {
+//     var respuesta = 'aaa';
+//     if (req.query.Descripcion) {
+//         next();
+
+//         getInfo(argv.direccion)
+//             //     .then(mensaje => console.log(mensaje))
+//             //     .catch(e => console.log(e));
+
+//     }
+//     res.send(req.query.Descripcion);
+//     //console.log(req);
+
+// });
+
+// var server = app.listen(8080, function() {
+//     console.log('Server is running ...');
+// });
